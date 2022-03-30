9c3bfb90e879e8f9c42b5f86588d83a0975104b7
Djamelikra <ikradesk@gmail.com>
2022-03-30 15:39:26 +0200

    3 dates

--- Patterns/DisplayDate/date.js
+++ Patterns/DisplayDate/date.js
@@ -6,26 +6,31 @@
 //   document.querySelector('p').innerHTML= date;
  
 
+/*------------------------------------US date----------------------------*/
+let dt = new Date();
+let strDate = dt.toLocaleDateString('en-US');
+document.querySelector('.dateUS').innerHTML = strDate;
+let opt = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
+strDate = dt.toLocaleDateString('en-US', opt);
+document.querySelector('.dateUSlong').innerHTML = strDate;
+document.querySelector('.us h3.slice').innerHTML = ('0'+(dt.getMonth()+1)).slice(-2)+"/"+ ('0'+dt.getDate()).slice(-2)+"/"+dt.getFullYear();
+/*------------------------------------Fr date----------------------------*/
 
+strDate = dt.toLocaleDateString('fr-Fr');
+document.querySelector('.dateFr').innerHTML = strDate;
+opt = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
+strDate = dt.toLocaleDateString('fr-FR', opt);
+document.querySelector('.dateFrlong').innerHTML = strDate;
+document.querySelector('.fr h3.slice').innerHTML =  ('0'+dt.getDate()).slice(-2)+"/"+('0'+(dt.getMonth()+1)).slice(-2)+"/"+dt.getFullYear();
 
+/*------------------------------------Arabic date----------------------------*/
 
-
-var date = new Date()
-document.querySelector('p.date').innerHTML =  ('0'+date.getDate()).slice(-2)+"/"+('0'+(date.getMonth()+1)).slice(-2)+"/"+date.getFullYear();
-
-
-var d = new Date();
-var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
-document.querySelector('p.date2').innerHTML = date;
-
-
-// document.querySelector('p.date').innerHTML = '0' + theDate.getDate().slice(-2)+'/' +('0' +
-//  theDate.getMonth()+1).slice(-2)+'/'+ theDate.getFullYear();
-
-
-
-//  var els = document.querySelector('p');
-// document.querySelector('p.show').innerHTML = 'retest___show'
+strDate = dt.toLocaleDateString('ar-EG');
+document.querySelector('.dateAr').innerHTML = strDate;
+opt = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
+strDate = dt.toLocaleDateString('ar-EG', opt);
+document.querySelector('.dateArlong').innerHTML = strDate;
+document.querySelector('.Ar h3.slice').innerHTML =  ('0'+dt.getDate()).slice(-2)+"/"+('0'+(dt.getMonth()+1)).slice(-2)+"/"+dt.getFullYear();
 
 
 
