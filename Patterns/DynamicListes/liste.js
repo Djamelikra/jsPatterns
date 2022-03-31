eb843010b0e119eeb0c6c7e25e2fff73193aa26d
Djamelikra <ikradesk@gmail.com>
2022-03-31 11:45:39 +0200

    add clock to listes

--- Patterns/DynamicListes/liste.js
+++ Patterns/DynamicListes/liste.js
@@ -38,20 +38,12 @@ function displayClock(){
 /*------------------------------------Fr date----------------------------*/
 let dt = new Date();
 let strDate = dt.toLocaleDateString('fr-Fr');
-document.querySelector('.dateFr').innerHTML = strDate;
+// document.querySelector('.dateFr').innerHTML = strDate;
 opt = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
 strDate = dt.toLocaleDateString('fr-FR', opt);
-document.querySelector('.dateFrlong').innerHTML = strDate;
-document.querySelector('.fr h3.slice').innerHTML =  ('0'+dt.getDate()).slice(-2)+"/"+('0'+(dt.getMonth()+1)).slice(-2)+"/"+dt.getFullYear();
+document.querySelector('p.dateFrlong').innerHTML = strDate;
+document.querySelector('p.slice').innerHTML =  ('0'+dt.getDate()).slice(-2)+"/"+('0'+(dt.getMonth()+1)).slice(-2)+"/"+dt.getFullYear();
 
-/*------------------------------------US date----------------------------*/
-
-strDate = dt.toLocaleDateString('en-US');
-document.querySelector('.dateUS').innerHTML = strDate;
-opt = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
-strDate = dt.toLocaleDateString('en-US', opt);
-document.querySelector('.dateUSlong').innerHTML = strDate;
-document.querySelector('.us h3.slice').innerHTML = ('0'+(dt.getMonth()+1)).slice(-2)+"/"+ ('0'+dt.getDate()).slice(-2)+"/"+dt.getFullYear();
 
 
 
