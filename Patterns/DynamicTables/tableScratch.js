2be158f78148bbe823af056f34bea36299b873d4
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-04-15 19:02:54 +0200

    tblV2

--- Patterns/DynamicTables/tableScratch.js
+++ Patterns/DynamicTables/tableScratch.js
@@ -1,5 +1,6 @@
 /*********************** */
 "use strict";
+/************************V0************************** */
 function genTableV0() {
   let tblZone = document.getElementById("tbl");
   let myTable = document.createElement("table");
@@ -27,7 +28,7 @@ function genTableV0() {
 //
 let ctaV0 = document.getElementsByName("btnV0")[0];
 ctaV0.addEventListener("click", genTableV0);
-////
+/************************V1************************** */
 function genTableV1() {
   //table location
   let tblZone = document.getElementById("tbl");
@@ -37,8 +38,8 @@ function genTableV1() {
   var tblBody = document.createElement("tbody");
 
   //number of row and column
-  let iRow = 3;
-  let jCol = 3;
+  let iRow = 5;
+  let jCol = 5;
 
   // creating all cells
   for (let i = 0; i < iRow; i++) {
@@ -47,21 +48,56 @@ function genTableV1() {
 
     for (let j = 0; j < jCol; j++) {
       var tdCell = document.createElement("td");
-      var cellText = document.createTextNode("i:" + i + " , j: " + j);
+      var cellText = document.createTextNode(
+        "i:" + (i + 1) + " , j: " + (j + 1)
+      );
       tdCell.appendChild(cellText);
       tblRow.appendChild(tdCell);
     }
     tblBody.appendChild(tblRow);
   }
   myTable.appendChild(tblBody);
-//append the table
+  //append the table
   tblZone.appendChild(myTable);
 
-  // add a few CSS
-  myTable.setAttribute('border', '2');
-  myTable.style.borderCollapse='collapse';
+  // add a few CSS STYLE ///////////
+  myTable.setAttribute("border", "2");
+  myTable.style.borderCollapse = "collapse";
   myTable.style.background = "orange";
+  myTable.style.width = "50%";
 }
 
-  let ctaV1 = document.getElementsByName('btnV1')[0];
-  ctaV1.addEventListener("click", genTableV1);
+let ctaV1 = document.getElementsByName("btnV1")[0];
+ctaV1.addEventListener("click", genTableV1);
+/************************V2************************** */
+
+function genTableV2() {
+  //table location
+  let tblZone = document.getElementById("tbl");
+
+    //init table
+    let myTable = document.createElement("table");
+
+  for (var i = 1; i < 5; i++) {
+    var tr = [] ;
+    tr[i] = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    td1.appendChild(document.createTextNode("Text1"));
+    td2.appendChild(document.createTextNode("Text2"));
+    tr[i].appendChild(td1);
+    tr[i].appendChild(td2);
+    myTable.appendChild(tr[i]);
+  }
+  tblZone.appendChild(myTable);
+  // add a few CSS STYLE ///////////
+  myTable.setAttribute("border", "2");
+  myTable.style.borderCollapse = "collapse";
+  myTable.style.background = "orange";
+  myTable.style.width = "50%";
+}
+
+let ctaV2 = document.getElementsByName("btnV2")[0];
+ctaV2.addEventListener("click", genTableV2);
+
+/************************V3************************** */
