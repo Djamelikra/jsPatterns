e6dd9158501335ecbe704c99ff6e938183fc1446
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-04-17 15:39:01 +0200

    table V5 full

--- Patterns/DynamicTables/tableScratch.js
+++ Patterns/DynamicTables/tableScratch.js
@@ -214,12 +214,48 @@ let theTable = document.getElementById('newTable');
 theTable.deleteRow(r.parentNode.parentNode.rowIndex);
   
 }
-// function genTableV5() {
-//   genTableVx();
-//   // document
-//   //   .getElementById("tbl")
-//   //   .appendChild(populateTable(null, 5, 3, "div padding text"));
-// }
 
 let ctaV5 = document.getElementsByName("btnV5")[0];
 ctaV5.addEventListener("click", genTableV5);
+
+/************************V6************************** */
+function genTableV6() {
+  //Build an array containing Customer records.
+  var customers = new Array();
+  customers.push(["Customer Id", "Name", "Country"]);
+  customers.push([1, "John Hammond", "United States"]);
+  customers.push([2, "Mudassar Khan", "India"]);
+  customers.push([3, "Suzanne Mathews", "France"]);
+  customers.push([4, "Robert Schidner", "Russia"]);
+
+  //Create a HTML Table element.
+  var table = document.createElement("TABLE");
+  table.border = "1";
+
+  //Get the count of columns.
+  var columnCount = customers[0].length;
+
+  //Add the header row.
+  var row = table.insertRow(-1);
+  for (var i = 0; i < columnCount; i++) {
+      var headerCell = document.createElement("TH");
+      headerCell.innerHTML = customers[0][i];
+      row.appendChild(headerCell);
+  }
+
+  //Add the data rows.
+  for (var i = 1; i < customers.length; i++) {
+      row = table.insertRow(-1);
+      for (var j = 0; j < columnCount; j++) {
+          var cell = row.insertCell(-1);
+          cell.innerHTML = customers[i][j];
+      }
+  }
+
+  var dvTable = document.getElementById("dvTable");
+  dvTable.innerHTML = "";
+  dvTable.appendChild(table);
+}
+
+let ctaV6 = document.getElementsByName("btnV6")[0];
+ctaV6.addEventListener("click", genTableV6);
