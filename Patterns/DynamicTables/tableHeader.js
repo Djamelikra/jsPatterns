8d79d33246deaedd562be70032e7d871b1df6359
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-04-18 13:48:03 +0200

    tbl+head+frm dyn

--- Patterns/DynamicTables/tableHeader.js
+++ Patterns/DynamicTables/tableHeader.js
@@ -32,14 +32,61 @@ function generateTable() {
   newTable.setAttribute("cellpadding", "10px");
   newTable.style.border = "outset 5px green";
 
-  //►4 add multiple rows dynamically with a pseudo form
+  //►4 add a form dynamically 
   addForm();
+  //►5 add multiple rows dynamically in the previous dynamic form
+  addBtnAdd();
+  addRow() ;
+
+}
+function removeRow(r) {
+  let theTable = document.getElementById("newTable");
+  theTable.deleteRow(r.parentNode.parentNode.rowIndex);
+}
+function addBtnAdd() {
+  var btnAddRow = document.createElement("button");
+  btnAddRow.setAttribute("type", "button");
+  btnAddRow.setAttribute("id", "btnAddRow");
+  btnAddRow.innerHTML = "Add a Row";
+  let parentNode = document.getElementById('newFrm');
+  parentNode.appendChild(btnAddRow);
+
+  // user.value = "";
+  // email.value = "";
+  // pwd.value = "";
+  
 }
 
 function addRow() {
-  let user = document.getElementById("user");
-  let email = document.getElementById("email");
-  let tel = document.getElementById("password");
+  document.getElementById("btnAddRow").addEventListener("click", function () {
+    let userName = document.getElementById("userName");
+    let email = document.getElementById("email");
+    let tel = document.getElementById("tel");
+    let birthDate = document.getElementById("birthDate");
+
+    let tr = newTable.insertRow(-1);
+
+    let tblDataArray = new Array();
+    tblDataArray = [user.value, email.value, pwd.value];
+
+    for (let i = 0; i < tblDataArray.length; i++) {
+      let td = tr.insertCell(-1);
+      td.innerHTML = tblDataArray[i];
+    }
+
+    let td = tr.insertCell(-1);
+    // add a button
+    var btnRemove = document.createElement("button");
+    btnRemove.setAttribute("type", "button");
+    btnRemove.innerHTML = "Remove";
+    btnRemove.setAttribute("onclick", "removeRow(this)");
+    td.appendChild(btnRemove);
+
+    userName.value = "";
+    email.value = "";
+    tel.value = "";
+    birthDate.value = "";
+  });
 }
 
 function addForm() {
@@ -52,7 +99,7 @@ function addForm() {
   lblUser.innerHTML = "User :";
   let iUser = document.createElement("input");
   iUser.setAttribute("type", "text");
-  iUser.setAttribute("name", "user");
+  iUser.setAttribute("id", "user");
   let brUser = document.createElement("br");
   let hrUser = document.createElement("hr");
 
@@ -61,7 +108,7 @@ function addForm() {
   lblEmail.innerHTML = "Email :";
   let iEmail = document.createElement("input");
   iEmail.setAttribute("type", "email");
-  iEmail.setAttribute("name", "email");
+  iEmail.setAttribute("id", "email");
   let brEmail = document.createElement("br");
   let hrEmail = document.createElement("hr");
 
@@ -70,7 +117,9 @@ function addForm() {
   lblPwd.innerHTML = "Password :";
   let iPwd = document.createElement("input");
   iPwd.setAttribute("type", "password");
-  iPwd.setAttribute("name", "pwd");
+  iPwd.setAttribute("id", "pwd");
+  let brPwd = document.createElement("br");
+  let hrPwd = document.createElement("hr");
 
   frm.appendChild(lblUser);
   frm.appendChild(iUser);
@@ -82,6 +131,8 @@ function addForm() {
   frm.appendChild(hrEmail);
   frm.appendChild(lblPwd);
   frm.appendChild(iPwd);
+  frm.appendChild(brPwd);
+  frm.appendChild(hrPwd);
 
 
   frm.style.width = '20%';
